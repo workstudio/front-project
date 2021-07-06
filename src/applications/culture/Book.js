730a40880e3b6dfca13e4d176b5a5a3ff3e493d7
iamwangcan <iamwangcan@163.com>
2021-07-06 10:10:13 +0800

    develop main

--- src/applications/culture/Book.js
+++ src/applications/culture/Book.js
@@ -5,14 +5,5 @@ conf.http.url = '/culture/books'
 export default class Book extends BaseModel {
   static entity = 'books'
 
-  static fields () {
-    return {
-      id: this.increment(),
-      name: this.string(''),
-      //datas: {},
-      status: this.string(''),
-    }
-  }
-
   static methodConf = conf
 }
