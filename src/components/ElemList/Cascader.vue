f9ce199c964d465677ebdfdb50b99421398730dd
job <job@job.com>
2022-06-15 19:49:44 +0800

    develop admin

---
+++ src/components/ElemList/Cascader.vue
@@ -0,0 +1,33 @@
+<template>
+  <span>
+  <el-cascader
+    ref="cascadersimple"
+    :options="infos"
+    :props="props"
+    placeholder="name"
+    :value="currentValue"
+    v-model="currentValue"
+    @change="handleRowChange"
+    clearable></el-cascader>
+  </el-form-item>
+  </span>
+</template>
+
+<script>
+import {column} from '@/applications/mixins/column';
+export default {
+  mixins: [column],
+  data() {
+    return {
+      disabled: false,
+      infos: this.row[this.field].infos,
+      props: this.row[this.field].props,
+      currentValue: this.row[this.field].valueSource,
+      rows: this.fieldItem.rowNum ? this.fieldItem.rowNum : 1,
+      withPop: this.fieldItem.withPop ? this.fieldItem.withPop : 0,
+    }
+  },
+  methods: {
+  }
+}
+</script>
